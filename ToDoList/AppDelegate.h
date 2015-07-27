085242f73b3efcb3f948878b47200faff2317bb8
Douglas P. Milstead <Douglas.P.Milstead@gmail.com>
2015-07-27 14:44:00 -0400

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Douglas P. Milstead on 7/27/15.
+//  Copyright (c) 2015 Douglas P. Milstead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
